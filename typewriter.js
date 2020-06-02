c1ca7aa96b81ea30b26b24288fdb2f3299314bb5
Andrew Delmar <andrewjdelmar@gmail.com>
2020-06-02 16:33:36 +0000

    updated comments

--- typewriter.js
+++ typewriter.js
@@ -2,10 +2,10 @@ const sentence = "hello there from lighthouse labs";
 const letterDelay = 100; 
 let i = 0;
 const typewriter = function () {
-  if (i < sentence.length) {
-    process.stdout.write(sentence.charAt(i));
-    i++;
-    setTimeout(typewriter, letterDelay);
+  if (i < sentence.length) { //makeshift for loop that:
+    process.stdout.write(sentence.charAt(i)); //'console logs' sentence at character [i]
+    i++; //increases by 1 char
+    setTimeout(typewriter, letterDelay); //runs on a delayed timer (function, delay)
   } else {
     console.log("");
   }
